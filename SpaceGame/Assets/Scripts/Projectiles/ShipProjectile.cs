617e5226c5ec9a2d79730bac723f6da8c11f523e
Felipe Morais <moraisfelipe97@gmail.com>
2020-11-17 01:59:11 -0300

    Implementação finalizada do vídeo 6

---
+++ SpaceGame/Assets/Scripts/Projectiles/ShipProjectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipProjectile : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed = 1f;
+    public float rate = 1f;
+    public float duration = 2f;
+    
+    [HideInInspector]
+    public GameObject shooter;
+
+    void Start()
+    {
+        GameObject.Destroy(gameObject, duration);
+    }
+
+    void Update()
+    {
+        transform.Translate(transform.up * Time.deltaTime * speed, Space.World);
+    }
+    /*
+    public void Shoot(Vector3 position)
+    {
+        GameObject proj = GameObject.Instantiate(projectile) as GameObject;
+        proj.transform.position = shooter.transform.position;
+        proj.transform.rotation = shooter.transform.rotation;
+
+    }
+    */
+
+}
---
+++ SpaceGame/Assets/Scripts/Projectiles/ShipShooter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipShooter : MonoBehaviour
+{
+    public List<ShipProjectile> projectiles;
+
+    private int selectedType = 0;
+
+    private bool shooted = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            StartCoroutine(Shoot());
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            selectedType = (selectedType + 1) % projectiles.Count;
+    }
+
+    public IEnumerator Shoot()
+    {
+        float t = projectiles[selectedType].rate;
+        if (shooted)
+            yield return null;
+        if (!shooted)
+        {
+            shooted = true;
+            yield return new WaitForSeconds(t);
+
+            GameObject proj = GameObject.Instantiate(projectiles[selectedType].gameObject) as GameObject;
+            proj.transform.position = transform.position;
+            proj.transform.rotation = transform.rotation;
+
+            shooted = false;
+        }
+
+    }
+}
