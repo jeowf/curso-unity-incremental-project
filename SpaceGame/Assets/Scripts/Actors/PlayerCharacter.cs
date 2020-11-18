70500edcd5f02c5f1f027ef73809955fa71a9e3e
kevinwall <kevinwall@ufrn.edu.br>
2020-11-18 17:44:16 -0300

    Implementando as coisas da aula 13

--- SpaceGame/Assets/Scripts/Actors/PlayerCharacter.cs
+++ SpaceGame/Assets/Scripts/Actors/PlayerCharacter.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour
 {
     public float movementSpeed = 340f;
+    // Modificação do jump
+    public float jumpSpeed = 120000f;
+    public LayerMask groundColision;
+    public float groundThresh = 0.2f;
+    // Fim modificação do jump
 
     private SpriteRenderer _sr;
     private Animator _anim;
@@ -15,6 +20,10 @@ public class PlayerCharacter : MonoBehaviour
 
     private float _horizontal;
     private float _vertical;
+    // Modificação do jump
+    private bool _jump;
+    private bool _grounded;
+    // Fim modificação do jump
 
     void Start()
     {
@@ -29,7 +38,14 @@ public class PlayerCharacter : MonoBehaviour
     {
         _horizontal = Input.GetAxis("Horizontal");
         _vertical = Input.GetAxis("Vertical");
+        
 
+        // Modificação do jump
+        if(Input.GetKeyDown("w"))
+        {
+            _jump = true;
+        }
+        // Fim modificação do jump
 
         if (_horizontal < 0 && !_flip)
         {
@@ -45,6 +61,16 @@ public class PlayerCharacter : MonoBehaviour
 
     void FixedUpdate()
     {
+        //Script do pulo
+        _grounded = Physics2D.Raycast(transform.position, -Vector3.up, groundThresh,groundColision);
+
+        if(_jump && _grounded)
+        {
+            _rb.AddForce(Vector2.up * jumpSpeed * Time.deltaTime, ForceMode2D.Force);
+            _jump = false;
+        }
+        // Fim do script do pulo
+
         //_rb.MovePosition(_rb.position + )
         if (_horizontal != 0)
             _rb.AddForce(Vector2.right * _horizontal * movementSpeed * Time.deltaTime, ForceMode2D.Force);
---
+++ SpaceGame/Assets/Scripts/Camera/PlataformFollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlataformFollowCamera : MonoBehaviour
+{
+    public Transform player;
+    public float followSpeed = 5f;
+    //public Vector3 offset;
+
+    void Awake()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    void LateUpdate ()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position, followSpeed * Time.deltaTime);
+    }
+}
